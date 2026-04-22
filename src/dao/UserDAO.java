a642e4b9c8f0cb27f8c9b860865ba0a295439044
IvaPandohova <ipandohova@gmail.com>
2026-04-22 22:55:39 +0300

    Update UserDAO.java

---
+++ src/dao/UserDAO.java
@@ -0,0 +1,17 @@
+package dao; 
+import db.DBConnection; 
+import java.sql.*; 
+public class UserDAO { 
+  public boolean login(String username, String password) { 
+    String sql = "SELECT * FROM users WHERE username=? AND password=?"; 
+    try(Connection conn = DBConnection.getConnection(); 
+        PreparedStatement ps = conn.prepareStatement(sql)){ 
+      ps.setString(1, username); 
+      ps.setString(2, password); 
+      ResultSet rs = ps.executeQuery(); 
+      return rs.next(); 
+    }
+      catch(Exception e){ e.printStackTrace(); } 
+    return false; 
+  } 
+}
