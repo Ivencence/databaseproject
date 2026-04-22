988c34daa3987d852f7dbf18579225f7c01eb2e3
IvaPandohova <ipandohova@gmail.com>
2026-04-22 23:08:58 +0300

    UserDAO.java

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -1,17 +1,48 @@
-package dao; 
-import db.DBConnection; 
-import java.sql.*; 
-public class UserDAO { 
-  public boolean login(String username, String password) { 
-    String sql = "SELECT * FROM users WHERE username=? AND password=?"; 
-    try(Connection conn = DBConnection.getConnection(); 
-        PreparedStatement ps = conn.prepareStatement(sql)){ 
-      ps.setString(1, username); 
-      ps.setString(2, password); 
-      ResultSet rs = ps.executeQuery(); 
-      return rs.next(); 
+package dao;
+
+import db.DBConnection;
+
+import java.security.MessageDigest;
+import java.sql.*;
+
+public class UserDAO {
+
+    public boolean login(String username, String password) {
+
+        String sql = "SELECT password FROM Users WHERE username=?";
+
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setString(1, username);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                String dbPass = rs.getString("password");
+                return dbPass.equals(hash(password));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false;
     }
-      catch(Exception e){ e.printStackTrace(); } 
-    return false; 
-  } 
+
+    public static String hash(String password) {
+
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hash = md.digest(password.getBytes());
+
+            StringBuilder sb = new StringBuilder();
+            for (byte b : hash) sb.append(String.format("%02x", b));
+
+            return sb.toString();
+
+        } catch (Exception e) {
+            return password;
+        }
+    }
 }
