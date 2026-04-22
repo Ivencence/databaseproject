7cbe0c402d957772d47761b0f69e40d8ee59bbca
ipandohova26 <ipandohova@gmail.com>
2026-04-22 21:45:35 +0300

    Initial commit - Swing MySQL Project

--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -1,25 +1,19 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
+package db;
+
+import java.sql.*;
 
 public class DBConnection {
-
     public static Connection getConnection() {
-
         try {
-
             Class.forName("com.mysql.cj.jdbc.Driver");
-
             return DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/architecture_db",
+                    "jdbc:mysql://localhost:3306/architecture",
                     "root",
-                    "My$ql_$erveR@2024"
+                    "password"
             );
-
         } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
-
     }
-
 }
